5a3f17acf4f20ea37c6eefefd263d3f3d1b110d3
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-05 18:20:03 +0530

    Check the user is already login or not

--- app/src/main/java/com/example/kontabai/Activity/WelcomeActivity.java
+++ app/src/main/java/com/example/kontabai/Activity/WelcomeActivity.java
@@ -1,21 +1,30 @@
 package com.example.kontabai.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.example.kontabai.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    DatabaseReference databaseReference;
+    String signed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        databaseReference=FirebaseDatabase.getInstance().getReference().child("OnlyUsers").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
     }
 
     @Override
@@ -29,16 +38,13 @@ public class WelcomeActivity extends AppCompatActivity {
                         | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                 finish();
             },1000);
-        }
-        if(firebaseUser!=null){
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(WelcomeActivity.this,MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                            | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    finish();
-                }
+        }  if(firebaseUser!=null){
+            handler.postDelayed(() -> {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                finish();
             },1000);
         }
+
     }
 }
